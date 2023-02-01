9e2f566688c78c1414a7175e5e59572b6878450f
EASYGOING45 <1505674928@qq.com>
2023-02-01 21:29:08 +0800

    日志库实现和Timestamp编写

--- Logger.cc
+++ Logger.cc
@@ -1,5 +1,5 @@
 #include "Logger.h"
-
+#include "Timestamp.h"
 #include <iostream>
 
 // 获取日志唯一的实例对象
@@ -13,4 +13,27 @@ Logger &Logger::instance()
 void Logger::setLogLevel(int level)
 {
     logLevel_ = level;
+}
+
+// 写日志 [级别信息] time : msg
+void Logger::log(std::string msg)
+{
+    switch (logLevel_)
+    {
+    case INFO:
+        std::cout << "[INFO]";
+        break;
+    case ERROR:
+        std::cout << "[ERROR]";
+        break;
+    case FATAL:
+        std::cout << "[FATAL]";
+        break;
+    case DEBUG:
+        std::cout << "[DEBUG]";
+        break;
+    default:
+        break;
+    }
+    std::cout << Timestamp::now().toString() << ":" << msg << std::endl;
 }
---
+++ build/CMakeFiles/EZmuduo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/huan/projects/EZMuduo/lib/libEZmuduo.pdb"
+  "/home/huan/projects/EZMuduo/lib/libEZmuduo.so"
+  "CMakeFiles/EZmuduo.dir/Logger.o"
+  "CMakeFiles/EZmuduo.dir/Logger.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EZmuduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
