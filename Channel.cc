519d03ec04af4dfb021a566996190f78eadfda3b
EASYGOING45 <1505674928@qq.com>
2023-02-03 21:30:00 +0800

    Channel.cc

---
+++ Channel.cc
@@ -0,0 +1,95 @@
+#include "Channel.h"
+// EventLoop待实现
+#include "Logger.h"
+
+#include <sys/epoll.h>
+
+const int Channel::kNoneEvent = 0;
+const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kWriteEvent = EPOLLOUT;
+
+// EventLoop : ChannelList Poller
+Channel::Channel(EventLoop *loop, int fd)
+    : loop_(loop), fd_(fd), events_(0), revents_(0), index_(-1), tied_(false)
+{
+}
+
+Channel::~Channel()
+{
+}
+
+// channel的tie方法何时调用？ 在一个TcpConnection新连接创建的时候 TCP Connection=》 Channel
+void Channel::tie(const std::shared_ptr<void> &obj)
+{
+    tie_ = obj;
+    tied_ = true;
+}
+
+/**
+ *当改变Channel所表示fd的events事件后，update负责在poller里面更改fd相应的事件 epoll_ctl
+ *EventLoop => ChannelList Poller
+ */
+void Channel::update()
+{
+    // 通过channel所属的eventloop，调用poller的相应方法，注册fd的events事件
+    // loop_->updateChannel(this);
+}
+
+// 在Channel所属的EventLoop中，删除当前的Channel
+void Channel::remove()
+{
+    // loop_->removeChannel(this);
+}
+
+// fd得到poller通知以后，处理事件
+void Channel::handleEvent(Timestamp receiveTime)
+{
+    if (tied_)
+    {
+        std::shared_ptr<void> guard = tie_.lock();
+        if (guard)
+        {
+            handleEventWithGuard(receiveTime);
+        }
+    }
+    else
+    {
+        handleEventWithGuard(receiveTime);
+    }
+}
+
+// 根据poller通知的channel发生的具体事件 由Channel负责调用具体的回调操作
+void Channel::handleEventWithGuard(Timestamp receiveTime)
+{
+    LOG_INFO("Channel HandleEvent Revents:%d\n", revents_);
+    if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN))
+    {
+        if (closeCallback_)
+        {
+            closeCallback_();
+        }
+    }
+    if (revents_ & EPOLLERR)
+    {
+        if (errorCallback_)
+        {
+            errorCallback_();
+        }
+    }
+
+    if (revents_ & (EPOLLIN | EPOLLPRI))
+    {
+        if (readCallback_)
+        {
+            readCallback_(receiveTime);
+        }
+    }
+
+    if (revents_ & EPOLLOUT)
+    {
+        if (writeCallback_)
+        {
+            writeCallback_();
+        }
+    }
+}
